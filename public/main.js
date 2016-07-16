51d13f4705906db8fcc8c5e88f21146c2f9b8676
Fernando Ortiz <fortiz2k@gmail.com>
2016-07-16 17:44:23 -0500

    Initial commit

---
+++ public/main.js
@@ -0,0 +1,39 @@
+// main.js
+var update = document.getElementById('update')
+update.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Darth Vader',
+      'quote': 'I find your lack of faith disturbing.'
+    })
+  })
+  .then(res => {
+    if (res.ok) return res.json()
+  })
+  .then(data => {
+    console.log(data)
+    window.location.reload(true)
+  })
+})
+
+var del = document.getElementById('delete')
+del.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'name': 'Darth Vader'
+    })
+  })
+  .then(res => {
+    if (res.ok) return res.json()
+  }).
+  then(data => {
+    console.log(data)
+    window.location.reload()
+  })
+})
---
+++ test.js
@@ -0,0 +1,3 @@
+var SlaveRegisterValid = require('./scraper').slave_register
+var png = require('./png').PNG
+png('123456781', 'public/images')
