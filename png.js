3ca594582a3858361403f2440fc6f65be9648cc3
Fernando Ortiz <fortiz2k@gmail.com>
2016-07-18 15:38:56 -0500

    remove linting errors for png

--- png.js
+++ png.js
@@ -1,39 +1,39 @@
-var SLRN = require('slrngen-js')
-var bwipjs = require('bwip-js')
-var images = require('images')
-var Canvas = require('canvas')
+var SLRN = require('slrngen-js');
+var bwipjs = require('bwip-js');
+var images = require('images');
+var Canvas = require('canvas');
 
 function generate(code, path) {
-  canvas = new Canvas(300,200)
-  ctx = canvas.getContext('2d')
+  var canvas = new Canvas(300, 200);
+  var ctx = canvas.getContext('2d');
   ctx.font = '20px Impact';
-  ctx.fillText('SLRN ' + SLRN.fmt(code), 22, 15)
-  //generate the SLRN code PNG that goes on top of the main barcode.
-  var img = images(canvas.toBuffer())
+  ctx.fillText('SLRN ' + SLRN.fmt(code), 22, 15);
+  // generate the SLRN code PNG that goes on top of the main barcode.
+  var img = images(canvas.toBuffer());
   bwipjs.toBuffer(
     {
-      bcid:        'ean13'         // Barcode type
-    , text:        SLRN.prefix() + code // Text to encode
-    , scale:       2               // 3x scaling factor
-    , height:      25              // Bar height, in millimeters
-    , includetext: true            // Show human-readable text
-    , paddingheight: 14
-//    , textxalign:  'center'        // Always good to set this
-//    , textfont:    'Inconsolata'   // Use your custom font
-//      textsize:      13              // Font size, in points
+      bcid: 'ean13', // Barcode type
+      text: SLRN.prefix() + code, // Text to encode
+      scale: 2, // 3x scaling factor
+      height: 25, // Bar height, in millimeters
+      includetext: true, // Show human-readable text
+      paddingheight: 14
+//    textxalign: 'center', // Always good to set this
+//    textfont: 'Inconsolata', // Use your custom font
+//    textsize: 13 // Font size, in points
     }, function (err, png) {
-      if (err) {
-        console.log(err)
-      } else {
-        images(png)
-          .draw(img, 5, 10)
-          .save(path + '/' + SLRN.fmt(code) + '.png', {
-            quality : 50
-          });
-      }
+    if (err) {
+      console.log(err);
+    } else {
+      images(png)
+        .draw(img, 5, 10)
+        .save(path + '/' + SLRN.fmt(code) + '.png', {
+          quality: 50
+        });
+    }
   });
 }
 
 module.exports = {
   PNG: generate
-}
+};
