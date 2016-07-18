356ce859b485edae023b038bf04c431085ba025c
Fernando Ortiz <fortiz2k@gmail.com>
2016-07-18 15:36:02 -0500

    remove linting errors

--- scraper.js
+++ scraper.js
@@ -1,51 +1,53 @@
-var request = require('request')
-var moment = require('moment')
+/* eslint camelcase: ["error", {properties: "never"}] */
+
+var request = require('request');
+var moment = require('moment');
 
 function formatDate(find, body, callback) {
-  var toscrape = body.substring(body.indexOf(find), body.indexOf(find)+100)
-  var scrapedDate = toscrape.substring(toscrape.indexOf('on the')+7, toscrape.indexOf('.'))
-  console.log('scrapedDate: ' + scrapedDate)
-  var dateSplit = scrapedDate.split(' ')
-  console.log('dateSplit: ' + dateSplit)
-  var day = dateSplit[0].substring(0,2)
-  console.log('day: ' + day)
-  var month = dateSplit[3]
-  console.log('month: ' + month)
-  var year = dateSplit[dateSplit.length-1]
-  console.log('year: ' + year)
-  var shortDate = moment(year + '-' + month + '-' + day, 'YYYY-MMMM-DD').format('YYYY-MM-DD')
-  callback(shortDate, scrapedDate)
+  var toscrape = body.substring(body.indexOf(find), body.indexOf(find) + 100);
+  var scrapedDate = toscrape.substring(toscrape.indexOf('on the') + 7, toscrape.indexOf('.'));
+  console.log('scrapedDate: ' + scrapedDate);
+  var dateSplit = scrapedDate.split(' ');
+  console.log('dateSplit: ' + dateSplit);
+  var day = dateSplit[0].substring(0, 2);
+  console.log('day: ' + day);
+  var month = dateSplit[3];
+  console.log('month: ' + month);
+  var year = dateSplit[dateSplit.length - 1];
+  console.log('year: ' + year);
+  var shortDate = moment(year + '-' + month + '-' + day, 'YYYY-MMMM-DD').format('YYYY-MM-DD');
+  callback(shortDate, scrapedDate);
 }
 
-function slave_register(code, callback) {
-  var url = 'http://www.slaveregister.com/' + code
+function slaveRegister(code, callback) {
+  var url = 'http://www.slaveregister.com/' + code;
   request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log('Scraping slaveregistry.com website for code ' + code)
-      var valid = -1
-      var msg = ''
-      var data = {slave_id: code, message: ''}
+    if (!error && response.statusCode === 200) {
+      console.log('Scraping slaveregistry.com website for code ' + code);
+      var valid = -1;
+      var msg = '';
+      var data = {slave_id: code, message: ''};
       if (body.includes(code + ' is not a valid Slave Registration Number')) {
-        msg = 'SlaveRegister.com: Entered code is invalid!'
-        data = {slave_id: code, message: msg}
-        valid = 1
+        msg = 'SlaveRegister.com: Entered code is invalid!';
+        data = {slave_id: code, message: msg};
+        valid = 1;
       } else if (body.includes(code + ' was issued by The Slave Register')) {
-        msg = 'SlaveRegister.com: Entered code is already used!'
-        valid = 2
-        var find = code + ' was issued by The Slave Register'
-        formatDate(find, body, function(shortDate, fullDate) {
-          data = {slave_id: code, message: msg, fullregdate: fullDate, regdate: shortDate}
-        })
+        msg = 'SlaveRegister.com: Entered code is already used!';
+        valid = 2;
+        var find = code + ' was issued by The Slave Register';
+        formatDate(find, body, function (shortDate, fullDate) {
+          data = {slave_id: code, message: msg, fullregdate: fullDate, regdate: shortDate};
+        });
       } else if (body.includes(code + ' was not registered')) {
-        msg = 'SlaveRegister.com: Entered number is valid but was never used!'
-        data = {slave_id: code, message: msg, fullregdate: moment(new Date(), 'DD-MMMM-YYYY'), regdate: moment(new Date()).format('YYYY-MM-DD')}
-        valid = 3
+        msg = 'SlaveRegister.com: Entered number is valid but was never used!';
+        data = {slave_id: code, message: msg, fullregdate: moment(new Date(), 'DD-MMMM-YYYY'), regdate: moment(new Date()).format('YYYY-MM-DD')};
+        valid = 3;
       }
-      callback(valid, data)
+      callback(valid, data);
     }
-  })
+  });
 }
 
 module.exports = {
-  SlaveRegister: slave_register
-}
+  SlaveRegister: slaveRegister
+};
--- server.js
+++ server.js
@@ -1,43 +1,44 @@
+/* eslint camelcase: ["error", {properties: "never"}] */
+
 const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const bodyParser = require('body-parser');
-const methodOverride = require('method-override')
-const errorhandler = require('errorhandler')
-const notifier = require('node-notifier')
+const methodOverride = require('method-override');
+const errorhandler = require('errorhandler');
+// const notifier = require('node-notifier');
 const favicon = require('serve-favicon');
-const moment = require('moment');
 const path = require('path');
-const SLRN = require('slrngen-js');
-const SlaveRegisterValid = require('./scraper').SlaveRegister;
+const slrn = require('slrngen-js');
+const slaveRegisterValid = require('./scraper').SlaveRegister;
 const png = require('./png').PNG;
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 if (process.env.NODE_ENV === 'development') {
   // only use in development
-  app.use(errorhandler({log: errorNotification}))
+  app.use(errorhandler({log: errorNotification}));
 }
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
 
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 // Set port
 app.set('port', process.env.PORT || 3000);
 
 function errorNotification(err, str, req) {
-  var title = 'Error in ' + req.method + ' ' + req.url
-
-  notifier.notify({
+  var title = 'Error in ' + req.method + ' ' + req.url + ': ' + err;
+  console.log(title);
+  /* notifier.notify({
     title: title,
     message: str
-  })
+  }); */
 }
 
 function createPNG(code) {
-  png(SLRN.defmt(code), path.resolve('./public/images'))
+  png(slrn.defmt(code), path.resolve('./public/images'));
 }
 
 var uristring =
@@ -47,112 +48,73 @@ var uristring =
 var db;
 
 MongoClient.connect(uristring, (err, database) => {
-  if (err) return console.log(err)
-  db = database
+  if (err) {
+    return console.log(err);
+  }
+  db = database;
   app.listen(app.get('port'), () => {
-    console.log('listening on ' + app.get('port'))
-  })
-})
+    console.log('listening on ' + app.get('port'));
+  });
+});
 
 app.get('/', (req, res) => {
- res.render('pages/index2')
-})
+  res.render('pages/index2');
+});
 
 app.get('/:id', (req, res) => {
-  db.collection('slaves').findOne({ slave_id: req.params.id }, (err, item) => {
-    if (err) return console.log(err)
+  db.collection('slaves').findOne({slave_id: req.params.id}, (err, item) => {
+    if (err) {
+      return console.log(err);
+    }
 
     if (item) {
-      res.render('pages/viewcert', {slave: item})
+      res.render('pages/viewcert', {slave: item});
     } else {
-      //TODO: check the current slaveregister.com to see if the record exist.
-      SlaveRegisterValid(req.params.id, function(valid, data) {
+      slaveRegisterValid(req.params.id, function (valid, data) {
         if (valid === 3) {
-          console.log('message:add new: ' + data.message)
-          res.render('pages/newinfo', {slave: data})
+          console.log('message:add new: ' + data.message);
+          res.render('pages/newinfo', {slave: data});
         } else if (valid === 2) {
-            console.log('message:add existing: ' + data.message)
-            res.render('pages/newinfo', {slave: data})
+          console.log('message:add existing: ' + data.message);
+          res.render('pages/newinfo', {slave: data});
+        } else {
+          res.render('pages/error', {slave: data});
         }
-        else {
-          res.render('pages/error', {slave: data})
-        }
-      })
+      });
     }
-  })
-})
+  });
+});
 
 app.get('/:id/cert', (req, res) => {
-  res.render('pages/error', {slave: {slave_id: req.params.id, message: 'Not implemented!'}})
-})
+  res.render('pages/error', {slave: {slave_id: req.params.id, message: 'Not implemented!'}});
+});
 
 app.post('/:id/register', (req, res) => {
   db.collection('slaves').save(req.body, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    console.log(req.body)
-    createPNG(req.params.id)
-    res.render('pages/viewcert', {slave: req.body})
-  })
-})
+    if (err) {
+      return console.log(err);
+    }
+    console.log('saved to database');
+    console.log(req.body);
+    console.log(result);
+    createPNG(req.params.id);
+    res.render('pages/viewcert', {slave: req.body});
+  });
+});
 
 app.get('/i/register', (req, res) => {
-  var id = SLRN.create();
-  var newID = SLRN.fmt(id);
-  //TODO: check number if already exist in slaveregister.com and or fetlife.com
-  SlaveRegisterValid(newID, function(valid, data) {
+  var id = slrn.create();
+  var newID = slrn.fmt(id);
+  slaveRegisterValid(newID, function (valid, data) {
     if (valid === 3) {
-      console.log('message:register ' + data.message)
-      res.render('pages/newinfo', {slave: data})
+      console.log('message:register ' + data.message);
+      res.render('pages/newinfo', {slave: data});
+    } else {
+      res.render('pages/error', {slave: data});
     }
-    else {
-      res.render('pages/error', {slave: data})
-    }
-  })
-})
+  });
+});
 
 app.get('/i/about', (req, res) => {
-  res.render('pages/error', {slave: {message: 'Not implemented!'}})
-})
-//app.get('/', (req, res) => {
-//  db.collection('quotes').find().toArray((err, result) => {
-//    if (err) return console.log(err)
-    // renders index.ejs
-//    res.render('index.ejs', {quotes: result})
-//  })
-//})
-
-app.post('/quotes', (req, res) => {
-  db.collection('quotes').save(req.body, (err, result) => {
-    if (err) return console.log(err)
-
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
-
-app.put('/quotes', (req, res) => {
-  db.collection('quotes').findOneAndUpdate({name: 'Yoda'}, {
-    $set: {
-      name: req.body.name,
-      quote: req.body.quote
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    console.log('Darth Vader updated Yodas quote.')
-    res.send(result)
-  })
-})
-
-app.delete('/quotes', (req, res) => {
-  db.collection('quotes').findOneAndDelete({name: req.body.name},
-  (err, result) => {
-    if (err) return res.send(500, err)
-    console.log('Darth Vader quote deleted.')
-    //res.send('A darth vadar quote got deleted')
-    res.send(result)
-  })
-})
+  res.render('pages/error', {slave: {message: 'Not implemented!'}});
+});
